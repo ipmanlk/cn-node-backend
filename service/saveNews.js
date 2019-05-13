0eb889cc0ca57d6dd82bf466e647df22c41fc5d9
ipmanlk <io@navinda.xyz>
2019-05-13 23:40:26 +0530

    fix: news items with empty titles show up

--- service/saveNews.js
+++ service/saveNews.js
@@ -4,13 +4,15 @@ const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('../db/cn.db');
 
 const save = (news) => {
-    db.run(`INSERT INTO ${news.lang}(source, title, post, link) VALUES(?,?,?,?)`, [`${news.source}`,`${news.title}`,`${news.post}`,`${news.link}`], function (err) {
-        if (err) {
-            return console.log(err.message);
-        }
-        // get the last insert id
-        console.log(`A row has been inserted with rowid ${this.lastID}`);
-    });
+    if (news.title !== "") {
+        db.run(`INSERT INTO ${news.lang}(source, title, post, link) VALUES(?,?,?,?)`, [`${news.source}`, `${news.title}`, `${news.post}`, `${news.link}`], function (err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            // get the last insert id
+            console.log(`A row has been inserted with rowid ${this.lastID}`);
+        });
+    }
 };
 
 module.exports = {
