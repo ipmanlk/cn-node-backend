d1f9667ce76100def82f3d73013825169bcd6611
Navinda <io@navinda.xyz>
2019-08-14 11:31:42 +0530

    add: clean news fuction when saving to db

--- service/saveNews.js
+++ service/saveNews.js
@@ -1,9 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
+const cheerio = require('cheerio');
 
 // load database
 let db = new sqlite3.Database('../db/cn.db');
 
-const save = (news) => {
+const save = (newsData) => {
+    news = clean(newsData);
     if (news.title !== "") {
         db.run(`INSERT INTO ${news.lang}(source, title, post, link) VALUES(?,?,?,?)`, [`${news.source}`, `${news.title}`, `${news.post}`, `${news.link}`], function (err) {
             if (err) {
@@ -13,6 +15,14 @@ const save = (news) => {
             console.log(`A row has been inserted with rowid ${this.lastID}`);
         });
     }
+};
+
+const clean = (news) => {
+    const $ = cheerio.load(news.post);
+    // remove ada feed junk
+    $('.sr-date').parent().remove();
+    news.post = $.html();
+    return news;
 };
 
 module.exports = {
